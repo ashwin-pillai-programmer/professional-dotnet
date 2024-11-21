0e22c8b25565ac3525ba2f81bff83b3c6731fd93
Ashwin Pillai <iamashp17@gmail.com>
2024-11-21 14:43:58 +0530

    Adding 2 new projects to the solution "Chapter_03"

---
+++ chapter_03/UsingTupleWithThreeValues_01/Program.cs
@@ -0,0 +1,21 @@
+﻿// Program to create Three values using tuples with C# in Visual Studio.
+// Programmer : Ashwin Pillai
+
+namespace UsingTupleWithThreeValues_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program to create Three values using tuples with C# in Visual Studio.\n");
+
+            // Creating a Tuple with three values: int, string, and double.
+            Tuple<int, string, double> student = new Tuple<int, string, double>(1, "Tom", 80.75);
+
+            // Accessing the three elements of the tuple.
+            Console.WriteLine($"Student ID: {student.Item1}");
+            Console.WriteLine($"Student Name: {student.Item2}");
+            Console.WriteLine($"Student Percentage: {student.Item3}");
+        }
+    }
+}
